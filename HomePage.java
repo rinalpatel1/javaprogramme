d56080cc5d222a25cdd7d43cba9d8803924ac8be
rinal patel <rinalpatel2017@hotmail.com>
2021-10-09 14:34:13 +0100

    this is my first commit

---
+++ HomePage.java
@@ -0,0 +1,15 @@
+package com.demo.nopcommerce;
+
+public class HomePage {
+//Main method to execute the program
+    public static void main(String[] args) {
+        //Print statement to print some text.
+    System.out.println("My First Java Program");
+    }
+
+}
+
+
+
+
+
---
+++ LoginPage.java
@@ -0,0 +1,11 @@
+package com.demo.nopcommerce;
+
+public class LoginPage {
+    //Main method to execute the programme
+    public static void main(String[] args) {
+
+        //PrintName and surname in seperate lines
+        //system.out.println("learning git");
+        System.out.print(" Patel");
+    }
+}
